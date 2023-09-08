78de8168b28c24bee2cbc13d9f6a052eedf041ac
mordrax <joseph.ni@gmail.com>
2023-09-08 20:48:35 +1000

    fixed dying and walking by decoupling them

--- enemy.gd
+++ enemy.gd
@@ -1,22 +1,20 @@
 extends CharacterBody2D
+
+var speed = 105
 var animate = false
 var die = false
+
+var is_dying = false # set to true when the enemy is dying
+
 func _ready():
 	pass
 var screen_size
+
 func _process(delta):
 	screen_size = get_viewport_rect().size
-	$animate.animation = "die"
-	if $animate.is_playing():
-		die = true
-	if die == false:
-		if $attack.is_playing():
-			animate = true
-		if animate == false:
-			$animate.show()
-			$animate.animation = "walk"
-			$animate.play()
-			position.x += 3
+	
+	walking(delta)
+	
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 
@@ -27,7 +25,18 @@ func _on_attacktimer_timeout():
 	$animate.show()
 	animate = false# Replace with function body.
 
-
+func dying():
+	is_dying = true
+	$animate.animation = "die"
+	$animate.play()
+	
+func walking(delta):
+	if is_dying:
+		return
+		
+	$animate.animation = "walk"
+	$animate.play()
+	position.x += speed * delta
 
 
 
